8f06567d1a5a7e7536a8444fcc8daa35fedeaa50
Jade Abbott <jabbott@retrorabbit.co.za>
2018-11-11 00:00:22 +0200

    Added handy utility program to compress files into tensor2tensor required folders

---
+++ compress_datasets_for_t2t.py
@@ -0,0 +1,42 @@
+import tarfile
+import sys
+import os
+import glob
+
+if len(sys.argv) < 4:
+    print("usage: python compress_datasets_for_t2t.py <root_folder> <language1> <language2>")
+root_folder = sys.argv[1]
+L1 = sys.argv[2]
+L2 = sys.argv[3]
+types = ["train", "dev"]
+
+root_folder = os.path.join(root_folder, "%s_%s" % (L1, L2)) 
+
+train_output = '%s_%s.train.tar.gz' % (L1,L2)
+dev_output = '%s_%s.dev.tar.gz' % (L1,L2)
+
+train_path = os.path.join(root_folder, train_output)
+dev_path = os.path.join(root_folder, dev_output)
+
+# Remove old compressed files
+old = glob.glob(os.path.join(root_folder, '*.tar.gz'))
+for x in old:
+    os.remove(x)
+
+train_files = glob.glob(os.path.join(root_folder, '*.train.*'))
+dev_files = glob.glob(os.path.join(root_folder, "*.dev.*"))
+
+with tarfile.open(name=train_path, mode='w:gz') as tar_handle:
+    # Find all "trains"
+    for t in train_files:
+        n = t.split("/")[-1]
+        tar_handle.add(t, arcname=n)
+
+with tarfile.open(name=dev_path, mode='w:gz') as tar_handle:
+    # Find all "trains"
+    for t in dev_files:
+        n = t.split("/")[-1]
+        tar_handle.add(t, arcname=n)
+
+print("Tarballs %s and %s have been created" % (train_path, dev_path))
+
