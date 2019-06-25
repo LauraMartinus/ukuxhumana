50133faee944f9e7479b0612555f6019a82b3ac8
Jade Abbott <jade.zoe.abbott@gmail.com>
2019-06-25 17:00:51 +0000

    TPUs

--- t2t/run_t2t_train.sh
+++ t2t/run_t2t_train.sh
@@ -17,7 +17,9 @@ original_data_dir=$2
 original_data_prefix=$3
 source=en
 target=$4
-
+STORAGE_BUCKET=gs://ukuxhumana-1
+DATA_DIR=$STORAGE_BUCKET/translation
+TMP_DIR=/home/jade/tmp
 echo "============ T2T Training =========="
 echo " - Problem Name: $problem_name"
 echo " - DATA_DIR: $original_data_dir"
@@ -26,50 +28,55 @@ echo " - Source Language: $source"
 echo " - Target Language: $target"
 echo "   "
 
-mkdir -p "/tmp/t2t/$problem_name/data"
-mkdir -p "/tmp/t2t/$problem_name/output"
-mkdir -p "/tmp/t2t/$problem_name/tmp"
-mkdir -p "/tmp/t2t/$problem_name/results"
+mkdir -p "$TMP_DIR/t2t/$problem_name/data"
+mkdir -p "$TMP_DIR/t2t/$problem_name/output"
+mkdir -p "$TMP_DIR/t2t/$problem_name/tmp"
+mkdir -p "$TMP_DIR/t2t/$problem_name/results"
 
 # This just speeds up things since no need to download
-cp $original_data_dir/$original_data_prefix.train.* /tmp/t2t/$problem_name/tmp
-cp $original_data_dir/$original_data_prefix.dev.* /tmp/t2t/$problem_name/tmp
-cp $original_data_dir/vocab*   /tmp/t2t/$problem_name/tmp
+cp $original_data_dir/$original_data_prefix.train.* $TMP_DIR/t2t/$problem_name/tmp/
+cp $original_data_dir/$original_data_prefix.dev.* $TMP_DIR/t2t/$problem_name/tmp/
+cp $original_data_dir/$original_data_prefix.test.*   $TMP_DIR/t2t/$problem_name/tmp/
+TPU_NAME=jade-zoe-abbott
 
 # Do training
 t2t-trainer \
   --generate_data \
-  --data_dir=/tmp/$problem_name/data \
-  --output_dir=/tmp/$problem_name/output \
-  --tmp_dir=/tmp/$problem_name/tmp \
+  --data_dir=$DATA_DIR/t2t/$problem_name/data \
+  --output_dir=$DATA_DIR/t2t/$problem_name/output \
+  --tmp_dir=$TMP_DIR/t2t/$problem_name/tmp \
   --problem=$problem_name \
   --model=transformer \
-  --hparams_set=transformer_base_single_gpu \
+  --hparams_set=transformer_tpu \
   --train_steps=125000 \
   --eval_steps=100 \
-  --t2t_usr_dir=./t2t/problems/ \
-  --warm_start_from=45000 
+  --t2t_usr_dir=~/ukuxhumana/t2t/problems/ \
+  --use_tpu=true \
+  --cloud_tpu_name=$TPU_NAME
 
+decode_from=$DATA_DIR/t2t/$problem_name/data/$original_data_prefix.test.$source
+decode_to=$DATA_DIR/t2t/$problem_name/results/$original_data_prefix.test.$target
+reference=$DATA_DIR/t2t/$problem_name/data/$original_data_prefix.test.$target
 
-decode_from=$original_data_dir/$original_data_prefix.test.$source
-decode_to=/tmp/t2t/$problem_name/results/$original_data_prefix.test.$target
-reference=$original_data_dir/$original_data_prefix.test.$target
-
-echo "============ T2T Decoding =========="
+#gsutil cp $TMP_DIR/t2t/$problem_name/tmp/$original_data_prefix.test.* $DATA_DIR/t2t/$problem_name/data/
+#echo "============ T2T Decoding =========="
 # Do decoding
-t2t-decoder \
-  --data_dir=/tmp/t2t/$problem_name/data \
-  --problem=$problem_name \
-  --model='transformer' \
-  --hparams_set='transformer_base_single_gpu' \
-  --output_dir=/tmp/t2t/$problem_name/output \
-  --decode_hparams="beam_size=4,alpha=0.6" \
-  --decode_from_file=$decode_from \
-  --decode_to_file=$decode_to
+#t2t-decoder \
+#  --data_dir=$DATA_DIR/t2t/$problem_name/data \
+#  --problem=$problem_name \
+#  --model=transformer \
+#  --hparams_set=transformer_tpu \
+#  --output_dir=$DATA_DIR/t2t/$problem_name/output \
+#  --decode_hparams="beam_size=4,alpha=0.6" \
+#  --decode_from_file=$decode_from \
+#  --decode_to_file=$decode_to \
+#  --use_tpu=true \
+#  --cloud_tpu_name=jade-zoe-abbott \
+#  --t2t_usr_dir=/home/jade/ukuxhumana/t2t/problems/ 
 
-
-# BLEU scoring
-echo "============ T2T BLEU =========="
-t2t-bleu \
-    --translation=$decode_to
-    --reference=$reference
+#gsutil cp $reference $TMP_DIR/t2t/$problem_name/data
+#gsutil cp $decode_to $TMP_DIR/t2t/$problem_name/results
+#echo "============ T2T BLEU =========="
+#t2t-bleu \
+#    --translation=$TMP_DIR/t2t/$problem_name/results/$original_data_prefix.test.$target
+#    --reference=$TMP_DIR/t2t/$problem_name/data/$original_data_prefix.test.$target
